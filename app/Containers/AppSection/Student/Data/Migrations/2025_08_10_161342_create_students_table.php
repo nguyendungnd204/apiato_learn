6643db5370e9a7a810bf434c7c1721343a5c6650
nguyendungnd204 <ngvandungnvd204@gmail.com>
2025-08-11 09:01:50 +0700

    update field's table students

--- app/Containers/AppSection/Student/Data/Migrations/2025_08_10_161342_create_students_table.php
+++ app/Containers/AppSection/Student/Data/Migrations/2025_08_10_161342_create_students_table.php
@@ -9,6 +9,17 @@ return new class() extends Migration {
     {
         Schema::create('students', static function (Blueprint $table) {
             $table->id();
+            $table->string('student_code')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('dob');
+            $table->enum('gender', ['male', 'female', 'other']);
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('class')->nullable();
+            $table->string('major')->nullable();
+            $table->date('enrollment_date')->nullable();
             $table->timestamps();
         });
     }
