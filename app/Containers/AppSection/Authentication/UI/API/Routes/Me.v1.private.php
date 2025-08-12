39e5f1ac4669c34b6bbc691ed0e8d1ab1b2e6e95
nguyendungnd204 <ngvandungnvd204@gmail.com>
2025-08-12 09:27:03 +0700

    remove dulipcate file

--- app/Containers/AppSection/Authentication/UI/API/Routes/Me.v1.private.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Containers\AppSection\Authentication\UI\API\Controllers\MeController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('me', MeController::class)
-    ->name('api_auth_me')
-    ->middleware(['auth:api']);
